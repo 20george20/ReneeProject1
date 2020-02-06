79167c8658524da59af99bff9049c4fe2a5e3bf8
20george <reneecgeorge@gmail.com>
2020-02-06 11:18:45 -0500

    feb 6, 11:17am

--- venv/first machine learning project.py
+++ venv/first machine learning project.py
@@ -19,7 +19,7 @@ for row in datareader2:
 for row in datareader3:
     dataWPWD.append(row)
 def getScore(row):
-    score = int(dataWPND[row][11])*3 + int(dataWPWD[row][11])*2 + int(dataNPND[row][11])
+    score = int(dataWPND[row][11].replace(",",""))*3 + int(dataWPWD[row][11].replace(",",""))*2 + int(dataNPND[row][11].replace(",",""))
     return score
 def getMaxScore():
     max = 0
@@ -29,9 +29,16 @@ def getMaxScore():
     return max
 def getList():
     list1 = []
-    for i in range(1, 52):
+    for i in range(1, 54):
         list2 = []
         list2.append(dataNPND[i][0])
         list2.append(getScore(i))
     list1.append(list2)
+    list1.sort(key=lambda x:x[1], reverse=True)
+    print "The top 10 worst States for animal testing are listed below"
+    for r in range(1, 10):
+        print list2[r][1]
+
+
+
 
